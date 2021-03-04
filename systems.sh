73943b9764a39cebe74b4bbfe88c2a79e3db84e5
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-03-04 18:02:19 +0100

    Add plot that compares systems.

---
+++ systems.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# Convert to common format
+$SOURCE_DIR/rumble2common.py   -i $SOURCE_DIR/rumble.csv    -o $SOURCE_DIR/rumble.common.json
+$SOURCE_DIR/bigquery2common.py -i $SOURCE_DIR/bigquery.json -o $SOURCE_DIR/bigquery.common.json
+
+# Join files
+rm $SOURCE_DIR/common.json
+for system in bigquery rumble
+do
+    cat $SOURCE_DIR/$system.common.json >> $SOURCE_DIR/common.json
+    echo >> $SOURCE_DIR/common.json
+done
+
+# Plot
+$SOURCE_DIR/systems.py -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems.pdf
