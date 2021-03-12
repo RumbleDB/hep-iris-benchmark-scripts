3393452c01c0a076d78950d5a1632c9793b82117
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-03-12 23:17:30 +0100

    Exclude Q6 from large input sizes.

--- presto/run_experiments.sh
+++ presto/run_experiments.sh
@@ -70,7 +70,12 @@ function run_many() {(
     done
 )}
 
-NUM_EVENTS=($(for l in {0..16}; do echo $((2**$l*1000)); done))
+NUM_EVENTS=($(for l in {0..11}; do echo $((2**$l*1000)); done))
 QUERY_IDS=($(for q in 1 2 3 4 5 6-1 6-2 7 8-1 8-2; do echo q$q; done))
+
+run_many NUM_EVENTS QUERY_IDS
+
+NUM_EVENTS=($(for l in {12..16}; do echo $((2**$l*1000)); done))
+QUERY_IDS=($(for q in 1 2 3 4 5 7 8-1 8-2; do echo q$q; done))
 
 run_many NUM_EVENTS QUERY_IDS
