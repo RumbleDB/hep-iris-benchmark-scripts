7656b1657164d2770e9833ae5a6dbed1dab78244
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-03-18 14:07:11 +0100

    Factor out data conversion into dedicated script.

---
+++ convert.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# Convert to common format
+$SOURCE_DIR/athena2common.py   -i $SOURCE_DIR/athena.json   -o $SOURCE_DIR/athena.common.json
+$SOURCE_DIR/bigquery2common.py -i $SOURCE_DIR/bigquery.json -o $SOURCE_DIR/bigquery.common.json
+$SOURCE_DIR/presto2common.py   -i $SOURCE_DIR/presto.json   -o $SOURCE_DIR/presto.common.json
+$SOURCE_DIR/rumble2common.py   -i $SOURCE_DIR/rumble.csv    -o $SOURCE_DIR/rumble.common.json
+
+# Join files
+rm $SOURCE_DIR/common.json
+for system in athena bigquery presto rumble
+do
+    cat $SOURCE_DIR/$system.common.json >> $SOURCE_DIR/common.json
+    echo >> $SOURCE_DIR/common.json
+done
