25782562483de437b09476060ce549679c4b7f5b
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-03-24 10:09:11 +0100

    Add plot with scan throughput.

--- systems.sh
+++ systems.sh
@@ -2,7 +2,8 @@
 
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-$SOURCE_DIR/systems-running-time.py  -l -x -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems-running-time.pdf
-$SOURCE_DIR/systems-cpu-time.py      -l -x -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems-cpu-time.pdf
-$SOURCE_DIR/systems-data-scanned.py     -x -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems-data-scanned.pdf
-$SOURCE_DIR/systems-price.py         -l    -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems-price.pdf
+$SOURCE_DIR/systems-running-time.py    -l -x -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems-running-time.pdf
+$SOURCE_DIR/systems-cpu-time.py        -l -x -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems-cpu-time.pdf
+$SOURCE_DIR/systems-data-scanned.py       -x -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems-data-scanned.pdf
+$SOURCE_DIR/systems-scan-throughput.py -l -x -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems-scan-throughput.pdf
+$SOURCE_DIR/systems-price.py           -l    -i $SOURCE_DIR/common.json -o $SOURCE_DIR/systems-price.pdf
