522516b5ac0a8126127260eee81687cda68fdedd
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-08-04 14:37:15 +0200

    Fix killing of SSH tunnel in terminate.sh scripts.

--- rumble/terminate.sh
+++ rumble/terminate.sh
@@ -10,7 +10,7 @@ experiments_dir="$SCRIPT_PATH/../experiments"
 deploy_dir="$(discover_cluster "$experiments_dir")"
 
 # Close SSH tunnel
-kill $(cat "$deploy_dir/tunnel.pid" || -1 ) || echo "Could not find SSH tunnel..."
+kill $(cat "$deploy_dir/tunnel.pid" || echo -1 ) || echo "Could not find SSH tunnel..."
 
 # Terminate
 terminate_cluster "$deploy_dir"
