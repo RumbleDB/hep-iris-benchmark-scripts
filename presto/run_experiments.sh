641e4958b2682aebdb80633710d17c1607e7945b
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-08-05 15:41:34 +0200

    [Presto] Fix configs that run in <10m on x24large.

--- presto/run_experiments.sh
+++ presto/run_experiments.sh
@@ -91,12 +91,17 @@ function run_many() {(
 
 NSF1=53446198
 
-NUM_EVENTS=($(for l in {0..11}; do echo $((2**$l*1000)); done))
+NUM_EVENTS=($(for l in {0..15}; do echo $((2**$l*1000)); done) $(for l in {0..0}; do echo $((2**$l*$NSF1)); done))
 QUERY_IDS=($(for q in 1 2 3 4 5 6-1 6-2 7 8; do echo query-$q; done))
 
 run_many NUM_EVENTS QUERY_IDS
 
-NUM_EVENTS=($(for l in {0..15}; do echo $((2**$l*1000)); done) $(for l in {0..4}; do echo $((2**$l*$NSF1)); done))
+NUM_EVENTS=($(for l in {0..15}; do echo $((2**$l*1000)); done) $(for l in {1..6}; do echo $((2**$l*$NSF1)); done))
 QUERY_IDS=($(for q in 1 2 3 4 5 7 8; do echo query-$q; done))
+
+run_many NUM_EVENTS QUERY_IDS
+
+NUM_EVENTS=($(for l in {7..7}; do echo $((2**$l*$NSF1)); done))
+QUERY_IDS=($(for q in 1 2 3 4 5; do echo query-$q; done))
 
 run_many NUM_EVENTS QUERY_IDS
