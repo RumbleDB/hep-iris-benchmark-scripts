bfa3a572cdaef57713557fe38a6c89eeaf5e3f82
DanGraur <dan.graur@inf.ethz.ch>
2021-08-07 22:01:23 +0200

    Added partial logic for deploying psql on aws.

---
+++ postgresql/deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+NUM_INSTANCES="${1:-1}"
+INSTANCE_TYPE="${2:-m5d.xlarge}"
+
+# Load common functions
+. "$SCRIPT_PATH/../common/ec2-helpers.sh"
+
+# Deploy cluster
+experiments_dir="$SCRIPT_PATH/../experiments"
+mkdir -p "$experiments_dir"
+deploy_cluster "$experiments_dir" $NUM_INSTANCES $INSTANCE_TYPE
+
+Deploy software on machines
+echo "Deploying software..."
+for dnsname in ${dnsnames[*]}
+do
+    (
+        (
+            scp -r "$SCRIPT_PATH/queries/scripts" ec2-user@$dnsname:/data
+            ssh -q ec2-user@$dnsname "bash -s" < "$SCRIPT_PATH"/environment.sh
+        ) &>> "$deploy_dir/deploy_$dnsname.log"
+        echo "Done deploying $dnsname."
+    ) &
+done
+wait
+echo "Done deploying machines."
