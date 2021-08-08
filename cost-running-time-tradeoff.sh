b01284e3533a516a017cce8d7d11aa28333e6979
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-08-08 13:09:05 +0200

    Accomodate AsterixDB in cost/performance plot.

--- cost-running-time-tradeoff.sh
+++ cost-running-time-tradeoff.sh
@@ -4,12 +4,16 @@ SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 for q in 1 2 3 4 5 6-1 6-2 7 8
 do
-    flags=""
+    flags="-l"
     if [[ "$q" != "1" && "$q" != "5" ]]; then flags="$flags -y"; fi
-    if [[ "$q" != "4" ]]; then flags="$flags -l"; fi
     if [[ "$q" -le 4 ]]; then flags="$flags -x"; fi
     $SOURCE_DIR/cost-running-time-tradeoff.py \
         -q $q $flags \
         -i $SOURCE_DIR/common.json \
         -o $SOURCE_DIR/cost-running-time-tradeoff-$q.pdf
 done
+
+$SOURCE_DIR/cost-running-time-tradeoff.py \
+    --legend \
+    -i $SOURCE_DIR/common.json \
+    -o $SOURCE_DIR/cost-running-time-tradeoff-legend.pdf
