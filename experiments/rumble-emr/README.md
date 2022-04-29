bdef27d0ace1c9e1d2d01a409ff75895f9cadd3f
DanGraur <dan.graur@inf.ethz.ch>
2022-04-29 17:04:59 +0200

    [rumble-emr] Added mention in readme regarding experiment plotting.

--- experiments/rumble-emr/README.md
+++ experiments/rumble-emr/README.md
@@ -9,8 +9,19 @@ In order to execute the HEP ADL benchmark queries with RumbleDB on AWS EMR one n
 1. Execute the `./run_experiments.sh` script (feel free to change what queries this runs; by default all queries shown in the paper are executed)
 1. The query execution data is dumped by default in `../experiments/rumbledb/experiment_yyyy-MM-dd-HH-mm-ss`. In this directory you will find:
     * A set of folders having the name `run_yyyy-MM-dd-HH-mm-ss.SSS` each containing detailed information on each of the queries executed during the experiment. 
-    * The `summary.jsonl` file which is obtained via the `summarize_experiment.py` source. This aggregates all the individual query statistics into one `.jsonl` type file. 
+    * The `summary.jsonl` file which is obtained via the `summarize_experiment.py` source. 
 1. Study or plot the above-mentioned  `summary.jsonl` file to see the query execution times
     * For the query execution time, one can scan the `runtime` entries
     * For the number of read bytes, one can scan the `inputBytes` or `bytesRead` entry 
 1. Terminate the cluster by running the `terminate.sh` script. Note that at one point you will be prompted to specify the new cluster state. You can press `^C` to exit this state.
+
+## Preparing the data for plotting
+
+To prepare the summary data for plotting, you'll have to use the following command in order to copy the `summary.jsonl` file to the plotting directory. You can do this via the following command:
+
+```
+lastexp=$(ls -d ../experiments/rumbledb/experiment_* | sort | tail -n1)
+cat "$lastexp/summary.json" >> "$(git rev-parse --show-toplevel)"/plots/rumble.json
+```
+
+Note that `lastexp=$(ls -d ../experiments/rumbledb/experiment_* | sort | tail -n1)` will fetch the path to the latest experiment directory. If you wish to pick another experiment directory make sure to change the path in `cat` accordingly.
--- experiments/rumble-emr/summarize_experiment.py
+++ experiments/rumble-emr/summarize_experiment.py
@@ -15,94 +15,94 @@ parser = argparse.ArgumentParser(description='Summarize the Rumble experiment.')
 parser.add_argument("experiment_dir", help="The experiment directory", type=str)
 
 def aggregate_run(path, save_file_name="metrics.json"):
-	# Read all the statistics dumps
-	with open(os.path.join(path, CONFIG), "r") as f:
-		summary = json.load(f)
+  # Read all the statistics dumps
+  with open(os.path.join(path, CONFIG), "r") as f:
+    summary = json.load(f)
 
-	with open(os.path.join(path, JOBS), "r") as f:
-		jobs = json.load(f)
+  with open(os.path.join(path, JOBS), "r") as f:
+    jobs = json.load(f)
 
-	with open(os.path.join(path, STAGES), "r") as f:
-		stages = json.load(f)
-	
-	# with open(os.path.join(path, SQL_JOBS), "r") as f:
-	# 	sql_jobs = json.load(f)
+  with open(os.path.join(path, STAGES), "r") as f:
+    stages = json.load(f)
+  
+  # with open(os.path.join(path, SQL_JOBS), "r") as f:
+  #   sql_jobs = json.load(f)
 
-	# Get the elapsed time
-	start_time = min([datetime.strptime(job["submissionTime"], 
-		"%Y-%m-%dT%H:%M:%S.%f%Z").timestamp() for job in jobs])
-	end_time = max([datetime.strptime(job["completionTime"], 
-		"%Y-%m-%dT%H:%M:%S.%f%Z").timestamp() for job in jobs])
-	summary["runtime"] = end_time - start_time
+  # Get the elapsed time
+  start_time = min([datetime.strptime(job["submissionTime"], 
+    "%Y-%m-%dT%H:%M:%S.%f%Z").timestamp() for job in jobs])
+  end_time = max([datetime.strptime(job["completionTime"], 
+    "%Y-%m-%dT%H:%M:%S.%f%Z").timestamp() for job in jobs])
+  summary["runtime"] = end_time - start_time
 
-	# Get the stage metrics
-	metrics = [
-		"executorDeserializeTime", 
-		"executorDeserializeCpuTime",
-		"executorRunTime",
-		"executorCpuTime",
-		"inputBytes",
-		"inputRecords",
-		"outputBytes",
-		"outputRecords"
-	]
+  # Get the stage metrics
+  metrics = [
+    "executorDeserializeTime", 
+    "executorDeserializeCpuTime",
+    "executorRunTime",
+    "executorCpuTime",
+    "inputBytes",
+    "inputRecords",
+    "outputBytes",
+    "outputRecords"
+  ]
 
-	deep_metrics = [
-		"bytesRead",
-		"recordsRead",
-		"bytesWritten",
-		"recordsWritten",
-		"maxInputRecords",
-		"maxInputRecordsBytes"
-	]
+  deep_metrics = [
+    "bytesRead",
+    "recordsRead",
+    "bytesWritten",
+    "recordsWritten",
+    "maxInputRecords",
+    "maxInputRecordsBytes"
+  ]
 
-	for metric in metrics + deep_metrics:
-		summary[metric] = 0
+  for metric in metrics + deep_metrics:
+    summary[metric] = 0
 
-	for stage in stages:
-		for metric in metrics:
-			summary[metric] += stage[metric]
+  for stage in stages:
+    for metric in metrics:
+      summary[metric] += stage[metric]
 
-		if summary["maxInputRecords"] < stage["inputRecords"] or \
-			(summary["maxInputRecords"] == stage["inputRecords"] and 
-			summary["maxInputRecordsBytes"] < stage["inputBytes"]):
-			summary["maxInputRecords"] = stage["inputRecords"]
-			summary["maxInputRecordsBytes"] = stage["inputBytes"]
+    if summary["maxInputRecords"] < stage["inputRecords"] or \
+      (summary["maxInputRecords"] == stage["inputRecords"] and 
+      summary["maxInputRecordsBytes"] < stage["inputBytes"]):
+      summary["maxInputRecords"] = stage["inputRecords"]
+      summary["maxInputRecordsBytes"] = stage["inputBytes"]
 
-		for _, task in stage["tasks"].items():
-			summary["bytesRead"] += task.get("taskMetrics", {}).get("inputMetrics", {}).get("bytesRead", 0)
-			summary["recordsRead"] += task.get("taskMetrics", {}).get("inputMetrics", {}).get("recordsRead", 0)
-			summary["bytesWritten"] += task.get("taskMetrics", {}).get("outputMetrics", {}).get("bytesWritten", 0)
-			summary["recordsWritten"] += task.get("taskMetrics", {}).get("outputMetrics", {}).get("recordsWritten", 0)
+    for _, task in stage["tasks"].items():
+      summary["bytesRead"] += task.get("taskMetrics", {}).get("inputMetrics", {}).get("bytesRead", 0)
+      summary["recordsRead"] += task.get("taskMetrics", {}).get("inputMetrics", {}).get("recordsRead", 0)
+      summary["bytesWritten"] += task.get("taskMetrics", {}).get("outputMetrics", {}).get("bytesWritten", 0)
+      summary["recordsWritten"] += task.get("taskMetrics", {}).get("outputMetrics", {}).get("recordsWritten", 0)
 
-	# Write and return 
-	with open(os.path.join(path, save_file_name), "w") as f:
-		json.dump(summary, f)
-		f.write("\n")
+  # Write and return 
+  with open(os.path.join(path, save_file_name), "w") as f:
+    json.dump(summary, f)
+    f.write("\n")
 
-	return summary
+  return summary
 
 
 def main(args):
-	summary = []
-	top_dir = args.experiment_dir
+  summary = []
+  top_dir = args.experiment_dir
 
-	for subdir in [os.path.join(top_dir, o) for o in os.listdir(top_dir) 
-		if os.path.isdir(os.path.join(top_dir, o))]:
-			try:
-				summary.append(aggregate_run(subdir))
-			except Exception as e:
-				print(f"Encountered an error at {subdir}", file=sys.stderr)
-				print(f" > Error: {e}", file=sys.stderr)
-				print(f" > Ignore this error in case this was a warmup run", file=sys.stderr)
+  for subdir in [os.path.join(top_dir, o) for o in os.listdir(top_dir) 
+    if os.path.isdir(os.path.join(top_dir, o))]:
+      try:
+        summary.append(aggregate_run(subdir))
+      except Exception as e:
+        print(f"Encountered an error at {subdir}", file=sys.stderr)
+        print(f" > Error: {e}", file=sys.stderr)
+        print(f" > Ignore this error in case this was a warmup run", file=sys.stderr)
 
-	with open(os.path.join(top_dir, "summary.jsonl"), "w") as f:
-		for j in summary:
-			json.dump(j, f)
-			f.write("\n")
+  with open(os.path.join(top_dir, "summary.json"), "w") as f:
+    for j in summary:
+      json.dump(j, f)
+      f.write("\n")
 
-	print("Done!")
+  print("Done!")
 
 
 if __name__ == '__main__':
-	main(parser.parse_args())
+  main(parser.parse_args())
