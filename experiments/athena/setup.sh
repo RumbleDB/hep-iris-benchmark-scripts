0253514c49257b0c1a275ce079af858eb2da9628
DanGraur <dan.graur@inf.ethz.ch>
2022-11-22 18:02:55 +0100

    Updated the Athena setup.sh

--- experiments/athena/setup.sh
+++ experiments/athena/setup.sh
@@ -1,36 +1,20 @@
+#!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="$SCRIPT_DIR/queries"
 
-ATHENA_DATASET=iris_hep_benchmark
-DATA_BUCKET_NAME=iris-hep-benchmark-data
-STAGING_DIR=s3://iris-hep-benchmark-data/tmp
+ATHENA_DATASET=iris_hep_benchmark  # This will have to be created by hand: https://docs.aws.amazon.com/athena/latest/ug/creating-databases.html
+DATA_BUCKET_NAME=hep-adl-ethz/artifact-evaluation/parquet/  # Set to your S3 data path
+STAGING_DIR=s3://<YOUR-ATHENA-STAGING-AREA> 
 
-for l in {0..16}
+NSF1=53446198
+
+for l in {0..16} $NSF1
 do
     n=$((2**$l*1000))
 
-    # Upload "shredded" data
-    aws s3 cp \
-        "$ROOT_DIR"/data/Run2012B_SingleMu_$n.parquet \
-        s3://$DATA_BUCKET_NAME/Run2012B_SingleMu_$n/Run2012B_SingleMu_$n.parquet
-
-    # Upload "native" data
-    aws s3 cp \
-        "$ROOT_DIR"/data/Run2012B_SingleMu_restructured_$n.parquet \
-        s3://$DATA_BUCKET_NAME/Run2012B_SingleMu_restructured_$n/Run2012B_SingleMu_restructured_$n.parquet
-
-    # Create "shredded" table and view
-    "$ROOT_DIR"/scripts/create_table.py \
-        --variant shredded \
-        --database $ATHENA_DATASET \
-        --staging-dir $STAGING_DIR \
-        --table-name Run2012B_SingleMu_$n \
-        --location s3://$DATA_BUCKET_NAME/Run2012B_SingleMu_$n/ \
-        --view-name Run2012B_SingleMu_${n}_view
-
     # Create "native" table
-    "$ROOT_DIR"/scripts/create_table.py \
+    python3 "$ROOT_DIR"/scripts/create_table.py \
         --variant native \
         --database $ATHENA_DATASET \
         --staging-dir $STAGING_DIR \
