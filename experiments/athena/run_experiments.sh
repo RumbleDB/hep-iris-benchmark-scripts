882e6c045c805e5854a1ee7e45f082747ba958c1
DanGraur <dan.graur@inf.ethz.ch>
2022-11-29 17:49:05 +0100

    [athena] Added --freeze-result parameter when executing queries to avoid false errors.

--- experiments/athena/run_experiments.sh
+++ experiments/athena/run_experiments.sh
@@ -46,7 +46,8 @@ function run_one {(
             --database "$ATHENA_DATABASE" \
             --input-table "$input_table" \
             --num-events $num_events \
-            --query-id $query_id
+            --query-id $query_id \
+            --freeze-result
         exit_code=$?
         echo "Exit code: $exit_code"
         echo $exit_code > "$run_dir"/exit_code.log
